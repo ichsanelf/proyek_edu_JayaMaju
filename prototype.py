a341b32c54a1933adaec714e5cbb4bee34457aca
ichsanelf <ichsanelf@gmail.com>
2025-01-13 07:44:05 +0700

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -6,27 +6,19 @@ import numpy as np
 model = joblib.load('model/RandomForest_model.joblib')
 st.markdown( """ <style> .title { text-align: center; } </style> """, unsafe_allow_html=True )
 
-
-st.markdown('<h1 class="title">Simple Prediction App</h1>', unsafe_allow_html=True)
-st.title("Jaya Jaya Maju")
-st.title("Prediksi Status Siswa")
+st.markdown('<h1 class="title">Jaya Jaya Maju</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="title">Prediksi Status Siswa</h1>', unsafe_allow_html=True)
 
 curricular_1st_sem_approved = st.number_input("Curricular First Semester Approved", min_value=0, max_value=30, step=1)
 curricular_1st_sem_grade = st.number_input("Curricular First Semester Grade", min_value=0, max_value=30, step=1)
 curricular_2nd_sem_approved = st.number_input("Curricular Second Semester Approved", min_value=0, max_value=30, step=1)
 curricular_2nd_sem_grade = st.number_input("Curricular Secod Semester Grade", min_value=0, max_value=30, step=1)
 
-
-if st.button("Predict"):
-    
-    input_data = np.array([[
-                            curricular_1st_sem_approved, curricular_1st_sem_grade,
-                            curricular_2nd_sem_approved, curricular_2nd_sem_grade]])
-    
+if st.button("Predict"):    
+    input_data = np.array([[curricular_1st_sem_approved, curricular_1st_sem_grade,
+                            curricular_2nd_sem_approved, curricular_2nd_sem_grade]])    
    
-    prediction = model.predict(input_data)  
-    
-   
+    prediction = model.predict(input_data)   
     st.write("Prediction :", prediction)
 
 
