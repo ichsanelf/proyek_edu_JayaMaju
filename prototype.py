6ae96cc914034720a6c25272c2d19cacd04f3a36
ichsanelf <ichsanelf@gmail.com>
2025-01-13 11:35:58 +0700

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,9 +1,19 @@
 import streamlit as st
+import gdown
 import joblib
 import numpy as np
 
-# Load the saved model
-model = joblib.load('model/RandomForest_model.joblib')
+#drive url for model
+url = https://drive.google.com/file/d/17WbUtbbR6xt1vjcUZK4Go2vEZaL2vYOn/view?usp=sharing
+
+#download model from drive
+output = "RandomForest_model.joblib"
+gdown.download(url, output, quiet=False)
+
+#load model and scaler
+model = joblib.load(output)
+scaler = joblib.load("model/scaler.joblib")
+
 st.markdown( """ <style> .title { text-align: center; } </style> """, unsafe_allow_html=True )
 
 st.markdown('<h1 class="title">Jaya Jaya Maju</h1>', unsafe_allow_html=True)
@@ -16,9 +26,9 @@ curricular_2nd_sem_grade = st.number_input("Curricular Secod Semester Grade", mi
 
 if st.button("Predict"):    
     input_data = np.array([[curricular_1st_sem_approved, curricular_1st_sem_grade,
-                            curricular_2nd_sem_approved, curricular_2nd_sem_grade]])    
-   
-    prediction = model.predict(input_data)   
+                            curricular_2nd_sem_approved, curricular_2nd_sem_grade]]) 
+    scaled_input = scaler.transform(input_data)
+    prediction = model.predict(scaled_input)    
     st.write("Prediction :", prediction)
 
 
